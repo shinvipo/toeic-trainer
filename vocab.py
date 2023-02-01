20c392f9710c30a81c71e56f8ece72165d71e46c
shinvipo <tranhai.22012000@gmail.com>
2023-02-02 01:31:26 +0700

    abcd

--- vocab.py
+++ vocab.py
@@ -181,4 +181,4 @@ def show_full_from_cache(word):
     return result
 
 if __name__ == "__main__":
-    print(show_bref(fetch_cambridge('the Apocalypse')))
+    print(fetch_cambridge('the Apocalypse'))
